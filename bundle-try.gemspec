333afe0498d2ec16a72acca97f300e4f72c7d9f4
Gert Goet <gert@thinkcreate.nl>
2015-05-19 10:26:28 +0200

    Thor help is never used

--- bundle-try.gemspec
+++ bundle-try.gemspec
@@ -27,6 +27,5 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "bundler", "~> 1.9"
-  spec.add_dependency "thor"
   spec.add_development_dependency "rake", "~> 10.0"
 end
--- lib/bundle-try.rb
+++ lib/bundle-try.rb
@@ -1,17 +1,1 @@
 require 'bundle_try/gemfile_generator'
-require 'thor'
-
-module BundleTry
-  class Cli < Thor
-    include Thor::Actions
-
-    desc "try [OPTIONS]", "Try a gem"
-    long_desc <<-D
-    Some reallt long description
-    D
-    #method_option "gemspec", :type => :string, :banner => "Use the specified .gemspec to create the Gemfile"
-    def help
-      puts "A helping hand"
-    end
-  end
-end
